f30d1ed11279ba2bbc921f22f9773497c25d08db
hiitstaco <aamirahalladeen@yahoe.com>
2024-11-25 19:37:41 -0500

    first upload

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <>
+      <h1 className="text-3xl">
+        Acct 343 Mock Final Exam
+      </h1>
+   </> 
+  )
+}
